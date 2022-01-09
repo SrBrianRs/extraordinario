52a40e5e9bfa5acde1dcc7eb5b8b4a0e07ed6568
SrBrianRs <srbrianrs@outlook.com>
2022-01-09 17:55:13 -0600

    añadiendo informacion del programa y test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
@@ -22,10 +23,31 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('extraordinario');
   });
 
-  it('should render title', () => {
+  it('Deberá tener escrito "Pruebas de software" en el h1', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('extraordinario app is running!');
+    const h1 = fixture.debugElement.query(By.css('h1')).nativeElement;
+    expect(h1.innerHTML).not.toBeNull();
+    expect(h1.textContent).toContain('Pruebas de software');
+  });
+
+  it('Debe tener contenido en el header', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const content = fixture.debugElement.query(By.css('header')).nativeElement;
+    expect(content.innerHTML).not.toBeNull();
+    expect(content.innerHTML.length).toBeGreaterThan(0);
+  });
+
+  it('Debe tener contenido en los section', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const content = fixture.debugElement.query(By.css('section')).nativeElement;
+    expect(content.innerHTML).not.toBeNull();
+    expect(content.innerHTML.length).toBeGreaterThan(0);
+  });
+
+  it('Debe tener contenido en el footer', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const content = fixture.debugElement.query(By.css('footer')).nativeElement;
+    expect(content.innerHTML).not.toBeNull();
+    expect(content.innerHTML.length).toBeGreaterThan(0);
   });
 });
